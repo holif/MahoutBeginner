3f346af6955effad2c6e3ce45ae5dea9a050604a
holif <1301297909@qq.com>
2016-10-06 22:59:57 +0800

    基于用户/物品的推荐

---
+++ Recommender/ItemBasedRecommender.java
@@ -0,0 +1,51 @@
+package recommender;
+
+import java.util.List;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
+import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.recommender.GenericItemBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.PearsonCorrelationSimilarity;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.ItemSimilarity;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+/**
+ * 基于物品推荐
+ * @progect Mahout_0.12.2
+ * @file ItemBasedRecommender.java
+ * @author holif
+ * @date 2016年10月6日
+ */
+public class ItemBasedRecommender {
+
+	public static void main(String[] args) {
+		MysqlDataSource dataSource = new MysqlDataSource();
+		dataSource.setServerName("localhost");
+		dataSource.setUser("root");
+		dataSource.setPassword("root");
+		dataSource.setDatabaseName("mahout");
+		MySQLJDBCDataModel dataModel = new MySQLJDBCDataModel(dataSource, "preftable", "user", "item", "pref", null);
+		try {
+			ItemSimilarity similarity = new PearsonCorrelationSimilarity(dataModel);
+			Recommender r = new GenericItemBasedRecommender(dataModel, similarity);
+			
+			LongPrimitiveIterator iter = dataModel.getUserIDs();
+		
+			while(iter.hasNext()){
+				long usid = iter.nextLong();
+				//includeKnownItems 是否推荐已知的物品
+				List<RecommendedItem> list = r.recommend(usid, 3, false);
+				System.out.print("usid["+usid+"]");
+				for (RecommendedItem recommendedItem : list) {
+					System.out.print(recommendedItem);
+				}
+				System.out.println();
+			}
+		} catch (TasteException e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ Recommender/UserBasedRecommender.java
@@ -0,0 +1,53 @@
+package recommender;
+import java.util.List;
+
+import org.apache.mahout.cf.taste.common.TasteException;
+import org.apache.mahout.cf.taste.impl.common.LongPrimitiveIterator;
+import org.apache.mahout.cf.taste.impl.model.jdbc.MySQLJDBCDataModel;
+import org.apache.mahout.cf.taste.impl.neighborhood.NearestNUserNeighborhood;
+import org.apache.mahout.cf.taste.impl.recommender.GenericUserBasedRecommender;
+import org.apache.mahout.cf.taste.impl.similarity.EuclideanDistanceSimilarity;
+import org.apache.mahout.cf.taste.recommender.RecommendedItem;
+import org.apache.mahout.cf.taste.recommender.Recommender;
+import org.apache.mahout.cf.taste.similarity.UserSimilarity;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+/**
+ * 基于用户推荐
+ * @progect Mahout_0.12.2
+ * @file UserBasedRecommender.java
+ * @author holif
+ * @date 2016年10月6日
+ */
+public class UserBasedRecommender {
+
+	public static void main(String[] args) {
+		// 基于数据库的数据
+		MysqlDataSource dataSource = new MysqlDataSource();
+		dataSource.setServerName("localhost");
+		dataSource.setUser("root");
+		dataSource.setPassword("root");
+		dataSource.setDatabaseName("mahout");
+
+		MySQLJDBCDataModel dataModel = new MySQLJDBCDataModel(dataSource, "preftable", "user", "item", "pref", null);
+	
+		try {
+			UserSimilarity userSimilarity = new EuclideanDistanceSimilarity(dataModel);
+			NearestNUserNeighborhood neighbor = new NearestNUserNeighborhood(2, userSimilarity, dataModel);
+			Recommender r = new GenericUserBasedRecommender(dataModel, neighbor, userSimilarity);
+			LongPrimitiveIterator iter = dataModel.getUserIDs();
+			
+			while(iter.hasNext()){
+				long usid = iter.nextLong();
+				List<RecommendedItem> list = r.recommend(usid, 3);//为用户usid推荐3个物品
+				System.out.print("usid["+usid+"]");
+				for (RecommendedItem recommendedItem : list) {
+					System.out.print(recommendedItem);
+				}
+				System.out.println();
+			}
+		} catch (TasteException e) {
+			e.printStackTrace();
+		}	
+	}
+}
