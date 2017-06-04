3506108d68f314b9b091dedf998241c808bb777e
holif <1301297909@qq.com>
2017-06-04 15:49:50 +0800

    修复切换用户后命令不执行

--- mr-recommender/hm-start.sh
+++ mr-recommender/hm-start.sh
@@ -12,8 +12,9 @@ cd /opt/
 
 #install jdk
 echo '***Install JDK***'
-wget -q "http://mirrors.linuxeye.com/jdk/jdk-8u112-linux-x64.tar.gz"
-tar zxf jdk-8u112-linux-x64.tar.gz
+wget "http://mirrors.linuxeye.com/jdk/jdk-8u112-linux-x64.tar.gz"
+tar zxvf jdk-8u112-linux-x64.tar.gz
+#set java environment variable
 echo 'export JAVA_HOME=/opt/jdk1.8.0_112' >> /etc/profile
 echo 'export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar' >> /etc/profile
 echo "export JAVA_OPTS='-Xms128m -Xmx256m'" >> /etc/profile
@@ -21,15 +22,17 @@ echo "export JAVA_OPTS='-Xms128m -Xmx256m'" >> /etc/profile
 
 #install hadoop
 echo '***Install Hadoop***'
-wget -q http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-2.8.0/hadoop-2.8.0.tar.gz 
-tar zxf hadoop-2.8.0.tar.gz 
+wget  http://mirrors.tuna.tsinghua.edu.cn/apache/hadoop/common/hadoop-2.8.0/hadoop-2.8.0.tar.gz 
+tar zxvf hadoop-2.8.0.tar.gz 
 mv hadoop-2.8.0 hadoop
 #download hadoop config file
-wget -q https://raw.githubusercontent.com/holif/MahoutBeginner/master/mr-recommender/core-site.xml
+wget  https://raw.githubusercontent.com/holif/MahoutBeginner/master/mr-recommender/core-site.xml
 mv core-site.xml /opt/hadoop/etc/hadoop/core-site.xml
 
-wget -q https://raw.githubusercontent.com/holif/MahoutBeginner/master/mr-recommender/hdfs-site.xml
+wget  https://raw.githubusercontent.com/holif/MahoutBeginner/master/mr-recommender/hdfs-site.xml
 mv hdfs-site.xml /opt/hadoop/etc/hadoop/hdfs-site.xml
+
+sed -i 's#${JAVA_HOME}#/opt/jdk1.8.0_112#g' /opt/hadoop/etc/hadoop/hadoop-env.sh
 
 chown -R hadoop:hadoop ./hadoop
 echo 'export HADOOP_HOME=/opt/hadoop' >> /etc/profile
@@ -37,10 +40,9 @@ echo 'export HADOOP_HOME_WARN_SUPPRESS=not_null' >> /etc/profile
 echo 'export CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath):$CLASSPATH' >> /etc/profile
 
 
-
 #install mahout
-wget -q https://mirrors.tuna.tsinghua.edu.cn/apache/mahout/0.12.2/apache-mahout-distribution-0.12.2.tar.gz
-tar zxf apache-mahout-distribution-0.12.2.tar.gz
+wget https://mirrors.tuna.tsinghua.edu.cn/apache/mahout/0.12.2/apache-mahout-distribution-0.12.2.tar.gz
+tar zxvf apache-mahout-distribution-0.12.2.tar.gz
 mv apache-mahout-distribution-0.12.2 mahout
 echo 'export MAHOUT_HOME=/opt/mahout' >> /etc/profile
 echo 'export MAHOUT_LOCAL=true' >> /etc/profile
@@ -50,11 +52,10 @@ echo 'export PATH=$PATH:$JAVA_HOME/bin:$HADOOP_HOME/bin:$HADOOP_HOME/sbin:$MAHOU
 chown -R hadoop:hadoop ./mahout
 source /etc/profile
 
-ssh-keygen -t rsa
-echo 'Input hadoop password'
-ssh-copy-id localhost
+su - hadoop<<EOF
 
-su hadoop
+ssh-keygen -t rsa
+ssh-copy-id localhost
 
 java -version
 echo '\n'
@@ -65,7 +66,5 @@ hadoop version
 echo '\n'
 
 mahout
-
-
 
 
